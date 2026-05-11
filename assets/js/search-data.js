92be6bcec1c5e65fa6b243472099baf3bde78ac8
alexandrebeaubien <33398580+alexandrebeaubien@users.noreply.github.com>
2026-05-11 21:00:44 +0000

    Deploying to gh-pages from @ alexandrebeaubien/alexandrebeaubien.github.io@582099f0dc4b3d27a8d7266d3b52783df667ec1a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "My projects",
+          description: "Under Construction!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
